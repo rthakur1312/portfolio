756a3ed1f28806c301300fa74c0c189abf2b64d6
rthakur1312 <67025716+rthakur1312@users.noreply.github.com>
2022-02-26 03:17:28 -0500

    first commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New Message on your portfolio'"; //letter subject
+	$to = 'rasheethakur29@gmail.com'; // Enter Your E-mail
+	$from = 'admin@rashithakur.tech'; // Admin e-mail
+	$fromName = 'Rashi portfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
